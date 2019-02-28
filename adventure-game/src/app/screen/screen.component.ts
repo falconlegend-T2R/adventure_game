85f57e6fc6f0651aa9f5b3aa661b3b8d8f7eec73
baneland <falconlegend@gmail.com>
2019-02-27 21:30:59 -0500

    made it pretty

--- adventure-game/src/app/screen/screen.component.ts
+++ adventure-game/src/app/screen/screen.component.ts
@@ -11,13 +11,38 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 // By default its set to Emulated.
 
 export class ScreenComponent implements OnInit {
-  story:any=[{
-    "page_number":0,
-    "text":"<h1>Hello world</h1>\n<p>This is page 1</p>",
-    "button1":1,
-    "button2":1,
-    "button3":3
-  }];
+  story:any={
+    '0':{
+      "text":"<p>You wake up in a dark place.Everything feels fuzzy...</p><p>Before you are able to take a step, a young man walks up to you. He seems distressed.</p><p>Can you hear me?</p>",
+      "btn1_text":'Yes',
+      "btn2_text":'Where am I?',
+      "btn3_text":'...',
+      "image":"assets/img/aladin_worried.gif",
+      "btn1":"0.a",
+      "btn2":"0.a",
+      "btn3":"0.b"
+  },
+  '0.a':{
+    "text":"<p>'You can talk! Hurray!'The boy jumped in excitement then quickly composed himself. </p><p>'I apologize....your the first person to talk back in days!'</p>",
+    "btn1_text":'Continue',
+    "btn2_text":'Continue',
+    "btn3_text":'Continue',
+    "image":"assets/img/aladin_happy.gif",
+    "btn1":1,
+    "btn2":1,
+    "btn3":1
+  },
+  '0.b':{
+    "text":"<p>His happiness was short lived. His tone turned serious</p><p>'I am not sure where I am either, or how I got here'</p><p>'I woke up not long ago.'</p>",
+    "btn1_text":'Continue',
+    "btn2_text":'Continue',
+    "btn3_text":'Continue',
+    "image":"assets/img/aladin_worried.gif",
+    "btn1":1,
+    "btn2":1,
+    "btn3":1
+  }
+};
   
   constructor() { }
 
